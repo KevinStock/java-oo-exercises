b3d741999f46251a50c2a48578aa7bd9433d883c
KevinStock <kevin@kevinstock.net>
2016-10-08 17:44:41 -0500

    added test for pickup and drop

--- java-track/exercises/RobotTest.java
+++ java-track/exercises/RobotTest.java
@@ -32,4 +32,30 @@ public class RobotTest {
 		assertTrue("Problem with distance", Math.round(r1.getDistance(r2) * 10000.0) / 10000.0 == 5.6569); 
 	}
 	
+	@Test
+	public void pickupHeavyTest() {
+		PackMuleRobot pmr1 = new PackMuleRobot("Kevin", new int[] {5, 5}, 3, 'N', 30.0);
+		assertFalse("Problem with picking up too heavy of an object", pmr1.pickup("Bowling Ball", 31));
+		assertTrue(pmr1.pickup("Sock", 10.0));
+		assertFalse(pmr1.pickup("Car", 21.0));
+	}
+	
+	@Test
+	public void pickupMultipleTest() {
+		PackMuleRobot pmr1 = new PackMuleRobot("Kevin", new int[] {5, 5}, 3, 'N', 30.0);
+		for (int i = 0; i < 30; i++) {
+			assertTrue("Problem picking up multiple objects", pmr1.pickup("Paper", 1.0));
+		}
+	}
+	
+	@Test
+	public void dropTest() {
+		PackMuleRobot pmr1 = new PackMuleRobot("Kevin", new int[] {5, 5}, 3, 'N', 30.0);
+		pmr1.pickup("Paper", 1);
+		pmr1.pickup("Pen", 2);
+		assertTrue("Problem dropping an object", pmr1.drop("Paper"));
+		assertFalse("Problem dropping an object not being carried", pmr1.drop("Sock"));
+	}
+	
+	
 }
