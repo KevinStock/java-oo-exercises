0962e51de3a00300567070dd3079b3af0a02f8ec
KevinStock <kevin@kevinstock.net>
2016-10-19 22:35:19 -0500

    added Entity class to hold users and posts with unique id

---
+++ java-track/studios/blogz/Entity.java
@@ -0,0 +1,30 @@
+package blogz;
+
+public abstract class Entity {
+
+	private int uid;
+	private static int uidCounter = 0;
+	protected String username;
+	protected String pwdHash;
+	
+	public Entity() {
+		uid = incrementUID();
+	}
+	
+	private int incrementUID() {
+		uidCounter++;
+		return uidCounter;
+	}
+	
+	public int getUID() {
+		return uid;
+	}
+	
+	public static void main(String[] args) {
+		Entity u = new User("Kevin", "123");
+		Entity p = new Post("First", "1st Post");
+		System.out.println(u);
+		System.out.println(p);
+	}
+	
+}
