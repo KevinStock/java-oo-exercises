d53a01bdedc240c14ef88bfc3114f3d628b85957
Kevin Stock <kevin@kevinstock.net>
2016-10-20 17:32:37 -0500

    tests added for user and post lists

--- java-track/studios/blogz/PostAndUserTest.java
+++ java-track/studios/blogz/PostAndUserTest.java
@@ -18,6 +18,18 @@ public class PostAndUserTest {
 		assertTrue(User.isValidPassword("123", "123"));
 		assertFalse(User.isValidPassword("123", "321"));
 	}
+	
+	@Test
+	public void userListTest() {
+		User u = new User("Kevin", "123");
+		assertTrue(User.getUsers().contains(u));
+	}
+	
+	@Test
+	public void postListTest() {
+		Post p = new Post("First", "1st Post");
+		Post.getPosts().contains(p);
+	}
 
 	
 }
