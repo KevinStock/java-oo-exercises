0333922bb8d790cdb349d53f140b1e5e074ec5a3
Kevin Stock <kevin@kevinstock.net>
2016-10-21 07:24:32 -0500

    uid property is now final added decrement method for cases where creating user fails

--- java-track/studios/blogz/Entity.java
+++ java-track/studios/blogz/Entity.java
@@ -2,7 +2,7 @@ package blogz;
 
 public abstract class Entity {
 
-	private int uid;
+	private final int uid;
 	private static int uidCounter = 0;
 	
 	public Entity() {
@@ -14,14 +14,20 @@ public abstract class Entity {
 		return uidCounter;
 	}
 	
+	protected static void decrementUID() {
+		uidCounter--;
+	}
+	
 	public int getUID() {
 		return uid;
 	}
 	
 	public static void main(String[] args) {
 		Entity u = new User("Kevin", "123");
+		Entity u2 = new User("?Kevin", "321");
 		Entity p = new Post("First", "1st Post");
 		System.out.println(u);
+		System.out.println(u2);
 		System.out.println(p);
 		System.out.println(User.getUsers());
 		System.out.println(Post.getPosts());
